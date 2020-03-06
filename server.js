5935615257443093114a0afa8a1ac7c65e2dbcd7
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-06 19:06:27 +0000

    Serve index.html at root, instead of index.ejs

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send('./public/index.html');
 });
 
 app.listen(PORT, () => {
