370fe2f9fa9295d848fab642a75bc79576547c56
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-06 20:57:13 +0000

    Open/close nav menu when navbar-burger is clicked, for mobile screens

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,12 @@
 $(() => {
+
+  // Open/close nav menu when navbar-burger is clicked
+  // For mobile only
+  $('.navbar-burger').click(() => {
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
+  });
+
   $.ajax({
     method: "GET",
     url: "/api/users"
