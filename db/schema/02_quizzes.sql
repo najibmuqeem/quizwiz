a1c5f52f678f6118e14330cfd4e64d126a9ccbf2
Najib <najib.muqeem@gmail.com>
2020-03-06 17:09:22 +0000

    added a quiz table

---
+++ db/schema/02_quizzes.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS quizzes CASCADE;
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  picture_url VARCHAR(255),
+  number_of_questions INTEGER NOT NULL,
+  number_of_plays INTEGER
+);
