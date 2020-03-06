ef062e0def74784f5814bcd8952997dfc8590f58
Najib <najib.muqeem@gmail.com>
2020-03-06 23:53:49 +0000

    created pool

--- db/database.js
+++ db/database.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
 
 // Users
 
