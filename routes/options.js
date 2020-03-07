c528ff02a4c13f1c63418ce7a405d69b890679ff
neethu-ms <msneethu2989@gmail.com>
2020-03-07 22:04:26 +0000

    Add routes for options

---
+++ routes/options.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const database = require('../db/database');
+
+module.exports = () => {
+  // Add  options
+  router.post('/', (req, res) =>  {
+    database.addOptionToQuestion(req.body.questionId,req.body.option, req.body.isCorrect).then(data => {
+      const option = data.rows[0];
+      res.json(option);
+    })
+      .catch(err => {
+        res.status(500).json({error: err.message});
+      });
+  });
+
+  // Get Options
+  router.get('/:id', (req, res) => {
+    database.getOptionsForQuestion(req.params.id).then(data => {
+      const options = data.rows;
+      res.json(options);
+    })
+      .catch(err => {
+        res.status(500).json({error: err.message});
+      });
+  });
+
+  return router;
+};
