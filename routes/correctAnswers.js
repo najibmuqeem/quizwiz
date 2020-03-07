5e6cfc32c8ace6ee06649abfe281a03f0ef83590
neethu-ms <msneethu2989@gmail.com>
2020-03-07 22:08:40 +0000

    Add route for receiving correct answers

--- routes/correctAnswers.js
+++ routes/correctAnswers.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const database = require('../db/database');
 module.exports = () => {
 //Get correct answers
-
   router.get('/:id', (req, res) => {
     database.getCorrectOptionsForQuiz(req.params.id).then(data => {
       const correctAnswers = data.rows;
