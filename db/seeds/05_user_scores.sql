dbbef5cd095368229885d24790db0b6aab1cb7a6
Najib <najib.muqeem@gmail.com>
2020-03-07 13:22:08 +0000

    make more seeds

--- db/seeds/05_user_scores.sql
+++ db/seeds/05_user_scores.sql
@@ -3,13 +3,27 @@ INSERT INTO user_scores (quiz_id, user_id, score) VALUES (1, 2, 2);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (1, 3, 3);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (1, 4, 4);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (1, 5, 5);
+
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (2, 1, 5);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (2, 2, 4);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (2, 3, 3);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (2, 4, 2);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (2, 5, 1);
+
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (3, 1, 3);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (3, 2, 1);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (3, 3, 4);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (3, 4, 5);
 INSERT INTO user_scores (quiz_id, user_id, score) VALUES (3, 5, 2);
+
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (4, 1, 2);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (4, 2, 3);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (4, 3, 5);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (4, 4, 1);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (4, 5, 4);
+
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (5, 1, 4);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (5, 2, 5);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (5, 3, 2);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (5, 4, 3);
+INSERT INTO user_scores (quiz_id, user_id, score) VALUES (5, 5, 3);
