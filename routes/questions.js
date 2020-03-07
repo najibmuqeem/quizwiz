e3c85632173cdab6d787f4ec909d17d5b576ff85
neethu-ms <msneethu2989@gmail.com>
2020-03-07 22:03:44 +0000

    Add routes for questions

--- routes/questions.js
+++ routes/questions.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
-// const database = require('../db/database');
-const {addQuestion, addOptions} = require('./helpers/questionHelper');
+const database = require('../db/database');
+
 module.exports = () => {
-  router.post('/', [addQuestion, addOptions]);
+
+  // Add question
+  router.post('/', (req, res) =>  {
+    database.addQuestionToQuiz(req.body.quizId,req.body.question).then(data => {
+      const question = data.rows[0];
+      res.json(question);
+    })
+      .catch(err => {
+        res.status(500).json({error: err.message});
+      });
+  });
+
+  // Get questions
+  router.get('/:id', (req, res) => {
+    database.getQuestionsForQuiz(req.params.id).then(data => {
+      const questions = data.rows;
+      res.json(questions);
+    })
+      .catch(err => {
+        res.status(500).json({error: err.message});
+      });
+  });
+
   return router;
 };
