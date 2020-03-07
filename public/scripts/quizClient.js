14763efc45134af297cee3c26e2bf9be45d741e5
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-07 20:25:13 +0000

    Move rendering functions to renderHelpers.js

--- public/scripts/quizClient.js
+++ public/scripts/quizClient.js
@@ -29,22 +29,3 @@ const fetchSingleQuiz = function() {
     dataType: "json"
   });
 };
-
-
-
-
-// Function to render quizzes
-const renderQuizzes = function(quizzes) {
-   console.log("rendered",quizzes);
-};
-
-
-
-// render specific quiz
-const renderQuiz = function(quiz) {
-
-};
-
-
-
-
