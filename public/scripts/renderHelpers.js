cefbe7a2875c0375e135dee27127be4e9c58a843
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-07 20:25:40 +0000

    Move rendering functions into new file

---
+++ public/scripts/renderHelpers.js
@@ -0,0 +1,11 @@
+// Function to render quizzes
+const renderQuizzes = function(quizzes) {
+  console.log("rendered",quizzes);
+};
+
+
+
+// render specific quiz
+const renderQuiz = function(quiz) {
+
+};
