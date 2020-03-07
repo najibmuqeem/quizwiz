7375202e47f760ca3d47d5c57a11d68d38aff0a6
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-07 21:48:24 +0000

    Call fetchAndRenderQuizzes when dom loads. Remove test ajax call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,12 +7,5 @@ $(() => {
     $('.navbar-menu').toggleClass('is-active');
   });
 
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  fetchAndRenderQuizzes();
 });
