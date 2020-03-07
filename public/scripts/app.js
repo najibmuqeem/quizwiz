7cf4c14f033a219efa25d14c742a50b840515b62
Najib <najib.muqeem@gmail.com>
2020-03-07 23:12:44 +0000

    fix variable names to be snake_cased

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,23 +6,24 @@ $(() => {
     $(".navbar-menu").toggleClass("is-active");
   });
 
-  fetchAndRenderQuizzes();
+  // fetchAndRenderQuizzes();
 
   //submit quiz
   $("#create-quiz").on("submit", e => {
     e.preventDefault();
+    e.stopPropagation();
 
     const title = $("#create-quiz")[0].title.value;
     const description = $("#create-quiz")[0].description.value;
-    const pictureURL = $("#create-quiz")[0].picture.value;
-    let isPublic;
+    const picture_url = $("#create-quiz")[0].picture.value;
+    let is_public;
     if ($("#public").prop("checked")) {
-      isPublic = true;
+      is_public = true;
     } else {
-      isPublic = false;
+      is_public = false;
     }
-    const numQuestions = $("#create-quiz")[0].questions.value;
-    const numOptions = $("#create-quiz")[0].options.value;
+    const number_of_questions = $("#create-quiz")[0].questions.value;
+    const number_of_options = $("#create-quiz")[0].options.value;
 
     $("#description").val("");
     $("#picture-url").val("");
@@ -35,17 +36,17 @@ $(() => {
     const quiz = {
       title,
       description,
-      pictureURL,
-      numQuestions,
-      isPublic,
-      userID: 1
+      picture_url,
+      number_of_questions,
+      is_public,
+      user_id: 1
     };
 
-    const quizID = createQuiz(quiz);
+    const quiz_id = createQuiz(quiz);
 
     let html = ``;
 
-    for (let i = 1; i <= numQuestions; i++) {
+    for (let i = 1; i <= number_of_questions; i++) {
       html += `
               <div class="field">
                 <label class="label">Question ${i}</label>
@@ -53,7 +54,7 @@ $(() => {
                   <input type="text" class="input question" />
                 </div>
               </div>`;
-      for (let j = 1; j <= numOptions; j++) {
+      for (let j = 1; j <= number_of_options; j++) {
         if (j === 1) {
           html += `
                   <div class="field">
@@ -84,7 +85,7 @@ $(() => {
             </div>
             <div>
               Quiz ID:
-              <span id="quiz-id">${quizID}</span>
+              <span id="quiz-id">${quiz_id}</span>
             </div>`;
     $("#questions").append(html);
   });
@@ -93,33 +94,33 @@ $(() => {
   $("#questions").on("submit", e => {
     e.preventDefault();
 
-    const numQuestions = $(".question").length;
-    const numOptions = $(".option").length;
-    const quizID = Number($("#quiz-id").val());
+    const number_of_questions = $(".question").length;
+    const number_of_options = $(".option").length;
+    const quiz_id = Number($("#quiz-id").val());
 
-    let isCorrect = false;
+    let is_correct = false;
     let question = "";
     let options = [];
     let counter = 1;
 
-    for (let i = 0; i < numQuestions; i++) {
+    for (let i = 0; i < number_of_questions; i++) {
       question = $(".question")[i].val();
-      let questionID = addQuestionToQuiz(quizID, question);
+      let question_id = addQuestionToQuiz(quiz_id, question);
       for (
         let j = counter - 1;
-        j < (counter * numOptions) / numQuestions;
+        j < (counter * number_of_options) / number_of_questions;
         j++
       ) {
         if ($(".option")[j].hasClass("correct")) {
-          isCorrect = true;
+          is_correct = true;
         } else {
-          isCorrect = false;
+          is_correct = false;
         }
         options.push($(".option")[j].val());
-        addOptionToQuestion(questionID, options[j], isCorrect);
+        addOptionToQuestion(question_id, options[j], is_correct);
       }
       options = [];
-      counter += numOptions / numQuestions;
+      counter += number_of_options / number_of_questions;
     }
   });
 });
