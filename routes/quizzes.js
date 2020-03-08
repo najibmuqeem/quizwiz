8141eaef2731ca62d0c4757482a92207c3c584fc
Najib <najib.muqeem@gmail.com>
2020-03-08 15:44:30 +0000

    fix to accept and return is_public parameter

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,48 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const database = require('../db/database');
+const database = require("../db/database");
 
 module.exports = () => {
-// Get quizzes
-  router.get('/', (req, res) => {
-    database.getQuizzes().then(data => {
-      const quizzes = data.rows;
-      res.json(quizzes);
-    })
-      .catch(err => {
-        res.status(500).json({error: err.message});
-      });
-  });
-
-  //Post quiz
-
-  router.post('/', (req,res) => {
-    console.log({ body: req.body });
-    database.createNewQuiz({
-      title: req.body.title,
-      description: req.body.description,
-      picture_url: req.body.picture_url,
-      number_of_questions: req.body.number_of_questions,
-      user_id: req.body.user_id
-    })
+  // Get quizzes
+  router.get("/", (req, res) => {
+    database
+      .getQuizzes()
       .then(data => {
         const quizzes = data.rows;
         res.json(quizzes);
       })
       .catch(err => {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  //Post quiz
+
+  router.post("/", (req, res) => {
+    console.log({ body: req.body });
+    database
+      .createNewQuiz({
+        title: req.body.title,
+        description: req.body.description,
+        picture_url: req.body.picture_url,
+        number_of_questions: req.body.number_of_questions,
+        user_id: req.body.user_id,
+        is_public: req.body.is_public
+      })
+      .then(data => {
+        const quizzes = data.rows;
+        res.json(quizzes);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   //Get a single quiz
 
-  router.get('/:id', (req, res) => {
-    database.getQuizzes(req.params.id).then(data => {
-      const  quiz = data.rows;
-      res.json(quiz);
-    })
+  router.get("/:id", (req, res) => {
+    database
+      .getQuizzes(req.params.id)
+      .then(data => {
+        const quiz = data.rows;
+        res.json(quiz);
+      })
       .catch(err => {
-        res.status(500).json({ error: err.message});
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
