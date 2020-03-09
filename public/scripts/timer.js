e5d4cdeec1900e7d985e2a7487bd5180f3d0a707
neethu-ms <msneethu2989@gmail.com>
2020-03-09 13:00:09 +0000

    Add timer

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -1,7 +1,65 @@
-const timer = function() {
-  let startTime = 1000 * 60 * 5;
-  setInterval(()=>{
+const timer = function(allottedTime) {
+  let startTime = Date.now();
+  let displaystring = "";
+  let allottedTimeCopy = allottedTime;
+  // Setting the initial value of timer
+  const hours = allottedTime / (1000 * 60 * 60);
+  allottedTime %= (1000 * 60 * 60);
+  const minutes = allottedTimeCopy / (1000 * 60);
+  allottedTimeCopy %= (1000 * 60);
+  const seconds = allottedTimeCopy / 1000;
+
+  if (hours >= 1) {
+    displaystring += Math.round(hours);
+  } else {
+    displaystring += 0;
+  }
+
+  if (minutes >= 1) {
+    displaystring += ":" + Math.round(minutes);
+  } else {
+    displaystring += ":" + 0;
+  }
+
+  if (seconds >= 1) {
+    displaystring += ":" + Math.round(seconds);
+  } else {
+    displaystring += ":" + 0;
+  }
+  console.log(displaystring); // Replace with jquery
+
+  // Updating timer after each second
+  const t = setInterval(() => {
     let currentTime = Date.now();
-    let remainingTime = currentTime - startTime;
-  },1000);
+    let usedTime = currentTime - startTime;
+    if (usedTime >= allottedTime) {
+      clearTimeout(t);
+    }
+    displaystring = "";
+    let remainingTime = allottedTime - usedTime;
+    console.log("remaining time:", remainingTime);
+    const hours = remainingTime / (1000 * 60 * 60);
+    remainingTime %= (1000 * 60 * 60);
+    const minutes = remainingTime / (1000 * 60);
+    remainingTime %= (1000 * 60);
+    const seconds = remainingTime / 1000;
+    if (hours >= 1) {
+      displaystring += Math.round(hours);
+    } else {
+      displaystring += 0;
+    }
+    if (minutes >= 1) {
+      displaystring += ":" + Math.round(minutes);
+    } else {
+      displaystring += ":" + 0;
+    }
+    if (seconds >= 0.5) {  // To avoid the time taken for computing the other statements in milli seconds
+      displaystring += ":" + Math.round(seconds);
+    } else {
+      displaystring += ":" + 0;
+    }
+    console.log(displaystring); // Replace with jquery
+  }, 1000);
 };
+
+timer(1000 * 10); // remove the invocation from here
