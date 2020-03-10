04f67c588788a82de4f6466aba8c7a2eab8ecf19
Najib <najib.muqeem@gmail.com>
2020-03-10 22:42:09 +0000

    add user to database if nonexistent

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const database = require("../db/database");
 
 module.exports = () => {
-//Set cookies
-  router.post('/', (req, res) => {
-    console.log('server api:',req.body.userName);
-    req.session.user_name = req.body.userName;
-    res.json(req.session.user_name);
+  //Set cookies
+  router.post("/", (req, res) => {
+    database.getUserWithUsername(req.body.userName).then(data => {
+      if (data.rowCount !== 0) {
+        req.session.user_name = req.body.userName;
+        res.json(req.session.user_name);
+      } else {
+        database
+          .addUser(req.body.userName)
+          .then(() => {
+            req.session.user_name = req.body.userName;
+            res.json(req.session.user_name);
+          })
+          .catch(err => res.status(500).json({ error: err.message }));
+      }
+    });
   });
 
   return router;
 };
-
