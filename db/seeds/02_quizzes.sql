f68079228d986d7b39dd4b2152d04d3f32f7a606
Najib <najib.muqeem@gmail.com>
2020-03-10 19:23:26 +0000

    more and more seeds

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -2,8 +2,10 @@ INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_
 
 INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Second Quiz', 'Exactly what it sounds like.', 'https://cdn.ebaumsworld.com/mediaFiles/picture/1035099/85708057.jpg', 5, 2);
 
-INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Najibs Nuts', 'Enjoy this amazing quiz on Najibs favorite nuts!', 'https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-61cfca772f4f991e157977ffe42febcb8c23d7dc-s800-c85.jpg', 5, 3);
+INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Najib''s Nuts', 'Enjoy this amazing quiz on Najibs favorite nuts!', 'https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-61cfca772f4f991e157977ffe42febcb8c23d7dc-s800-c85.jpg', 5, 3);
 
-INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Berks Barbies', 'Enjoy this amazing quiz on Berks favorite Barbies!', 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fsarahkim%2Ffiles%2F2019%2F02%2FFashionistas_PR_finalShot_v3-1200x675.jpg', 5, 4);
+INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Berk''s Barbies', 'Enjoy this amazing quiz on Berks favorite Barbies!', 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fsarahkim%2Ffiles%2F2019%2F02%2FFashionistas_PR_finalShot_v3-1200x675.jpg', 5, 4);
 
-INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Neethus Narwhals', 'Enjoy this amazing quiz on Neethus favorite Narwhals!', 'https://c402277.ssl.cf1.rackcdn.com/photos/14875/images/magazine_hero/Medium_WW248926.jpg?1513623521', 5, 5);
+INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('Neethu''s Narwhals', 'Enjoy this amazing quiz on Neethus favorite Narwhals!', 'https://c402277.ssl.cf1.rackcdn.com/photos/14875/images/magazine_hero/Medium_WW248926.jpg?1513623521', 5, 5);
+
+INSERT INTO quizzes (title, description, picture_url, number_of_questions, user_id) VALUES ('TRUMP 2020', 'OUR LORD AND SAVIOR', 'https://i1.wp.com/www.nationalreview.com/wp-content/uploads/2019/10/donald-trump-marine-one.jpg?fit=789%2C460&ssl=1', 5, 3);
