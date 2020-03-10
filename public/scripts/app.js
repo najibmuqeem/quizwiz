64faf9f8e3053f547aae38539817954258839ceb
Najib <najib.muqeem@gmail.com>
2020-03-10 21:04:21 +0000

    formatting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,22 @@
 $(() => {
+  fetchAndRenderQuizzes();
 
   // Grab any url query params. To be used when sharing quizzes
-  $.urlParam = function(name){
-    const results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  $.urlParam = function(name) {
+    const results = new RegExp("[?&]" + name + "=([^&#]*)").exec(
+      window.location.href
+    );
 
     if (results) {
       return results[1];
     } else {
       return null;
     }
-  }
+  };
 
   // Render a specific quiz page if a quiz query was in the URL
-  if ($.urlParam('quiz')) {
-    fetchSingleQuiz($.urlParam('quiz'));
+  if ($.urlParam("quiz")) {
+    fetchSingleQuiz($.urlParam("quiz"));
   }
 
   // Open/close nav menu when navbar-burger is clicked
@@ -24,14 +27,12 @@ $(() => {
   });
 
   // Initiate .share-button elements as a clipboard object
-  new ClipboardJS('.share-button');
+  new ClipboardJS(".share-button");
 
   // Notify user when sharing info is copied to clipboard
-  $('body').on('click', '.share-button', () => {
-    alert('Copied to clipboard!\nPaste anywhere to share this quiz!');
-  })
-
-  fetchAndRenderQuizzes();
+  $("body").on("click", ".share-button", () => {
+    alert("Copied to clipboard!\nPaste anywhere to share this quiz!");
+  });
 
   // cancel quiz button
   $("body").on("click", "#cancel-quiz", e => {
@@ -46,7 +47,6 @@ $(() => {
     removeQuiz(Number($("#quiz-id")[0].innerText));
     fetchAndRenderQuizzes();
   });
-
 
   $("body").on("submit", "#create-quiz", e => {
     e.preventDefault();
