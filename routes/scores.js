62973a071df3187e6320ca036ce5ed9b94db5493
Najib <najib.muqeem@gmail.com>
2020-03-11 16:37:21 +0000

    formatting

--- routes/scores.js
+++ routes/scores.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const database = require('../db/database');
+const database = require("../db/database");
 
 module.exports = () => {
-//Post scores
-  router.post('/', (req, res) => {
-    database.insertScore(req.body.quiz_id,req.body.user_id,req.body.score).then(data => {
-      const scores = data.rows;
-      res.json(scores);
-    })
+  //Post scores
+  router.post("/", (req, res) => {
+    database
+      .insertScore(req.body.quiz_id, req.body.user_id, req.body.score)
+      .then(data => {
+        const scores = data.rows;
+        res.json(scores);
+      })
       .catch(err => {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
       });
   });
-
 
   // Get scores
-  router.get('/:id/:qid', (req, res) => {
-    database.getScores(req.params.id,req.params.qid).then(data => {   /* req.params.id temporarily replaced by req.session['user_id']*/
-      const scores = data.rows;
-      res.json(scores);
-    })
+  router.get("/:id/:qid", (req, res) => {
+    database
+      .getScores(req.params.id, req.params.qid)
+      .then(data => {
+        /* req.params.id temporarily replaced by req.session['user_id']*/
+        const scores = data.rows;
+        res.json(scores);
+      })
       .catch(err => {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
       });
   });
-
 
   return router;
 };
