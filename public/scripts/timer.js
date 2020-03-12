f60d5de368d2f9c2ea76490ecdcb9b6cb099b0a2
neethu-ms <msneethu2989@gmail.com>
2020-03-12 22:12:15 +0000

    Refactor code

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -2,35 +2,16 @@ const timer = function(allottedTime, correctAnswer) {
   let startTime = Date.now();
   let displaystring = "";
   let allottedTimeCopy = allottedTime;
-  // Setting the initial value of timer
-  // const hours = allottedTime / (1000 * 60 * 60);
-  // allottedTime %= (1000 * 60 * 60);
-  // const minutes = allottedTimeCopy / (1000 * 60);
-  // allottedTimeCopy %= (1000 * 60);
   const seconds = allottedTimeCopy / 1000;
-
-  // if (hours >= 1) {
-  //   displaystring += Math.round(hours);
-  // } else {
-  //   displaystring += 0;
-  // }
-
-  // if (minutes >= 1) {
-  //   displaystring += ":" + Math.round(minutes);
-  // } else {
-  //   displaystring += ":" + 0;
-  // }
-
   if (seconds >= 1) {
     displaystring += Math.round(seconds);
   } else {
     displaystring += 0;
   }
-
   // Initial display of time
   if (!clickTrack) {
     $("#timerHeading").text(displaystring);
-    $("#timerHeading").css({color:'white'});
+    $("#timerHeading").css({ color: 'white' });
   }
   // Updating timer after each second
   const t = setInterval(() => {
@@ -38,35 +19,16 @@ const timer = function(allottedTime, correctAnswer) {
     let usedTime = currentTime - startTime;
     displaystring = "";
     let remainingTime = allottedTime - usedTime;
-
-    const hours = remainingTime / (1000 * 60 * 60);
-    remainingTime %= (1000 * 60 * 60);
-    const minutes = remainingTime / (1000 * 60);
-    remainingTime %= (1000 * 60);
     const seconds = remainingTime / 1000;
-    // if (hours >= 1) {
-    //   displaystring += Math.round(hours);
-    // } else {
-    //   displaystring += 0;
-    // }
-    // if (minutes >= 1) {
-    //   displaystring += ":" + Math.round(minutes);
-    // } else {
-    //   displaystring += ":" + 0;
-    // }
     if (seconds >= 0.5) {  // To avoid the time taken for computing the other statements in milli seconds
       displaystring += Math.round(seconds);
     } else {
       displaystring += 0;
     }
-
     // clear timer and go to next question
-    console.log('clickTrack',clickTrack);
-    console.log('cancelButton',cancelButton);
     if (usedTime >= allottedTime && !clickTrack && !cancelButton) {
       $("#timerHeading").text(displaystring);
       clearTimeout(t);
-
       $(".option").css("pointer-events", "none");
       $(".option")
         .filter(function() {
@@ -80,7 +42,6 @@ const timer = function(allottedTime, correctAnswer) {
       setTimeout(() => {
         renderQuestion(quizData);
       }, 1500);
-
       return;
     }
     //clear timer
@@ -89,7 +50,6 @@ const timer = function(allottedTime, correctAnswer) {
       clickTrack = false;
       return;
     }
-
     if (cancelButton) {
       clearTimeout(t);
       cancelButton = false;
@@ -97,21 +57,16 @@ const timer = function(allottedTime, correctAnswer) {
     }
     // Displaying time
     $("#timerHeading").text(displaystring);
-    $("#timerHeading").css({color:'white'});
-
+    $("#timerHeading").css({ color: 'white' });
     // Changing border color depending on time left
-    if (displaystring < 3) {
+    if (Number(displaystring) < 3) {
       $("#timerHeading").css('border-image', 'linear-gradient(180deg, rgb(170, 47, 17), rgb(209, 15, 9)) 1');
-
-    } else if (displaystring < 5) {
+    } else if (Number(displaystring) < 5) {
       $("#timerHeading").css('border-image', 'linear-gradient(180deg, rgb(170, 47, 17), rgb(221, 218, 40)) 1');
-
-    } else if (displaystring < 7) {
+    } else if (Number(displaystring) < 7) {
       $("#timerHeading").css('border-image', 'linear-gradient(180deg, rgb(219, 207, 41), rgb(221, 218, 40)) 1');
-
-    } else if (displaystring < 9) {
+    } else if (Number(displaystring) < 9) {
       $("#timerHeading").css('border-image', 'linear-gradient(180deg, rgb(219, 207, 41), rgb(27, 168, 69)) 1');
     }
-
   }, 1000);
 };
