a80c5d3dbf2d050498c99773dabb60af479d51d6
Najib <najib.muqeem@gmail.com>
2020-03-13 04:01:30 +0000

    implement high score

--- public/scripts/scoreClient.js
+++ public/scripts/scoreClient.js
@@ -9,7 +9,6 @@ const storeScore = function(quiz_id, user_id, score) {
 
 // Get high score
 const getHighScore = function(user_id, quiz_id) {
-  console.log("i'm getting called");
   $.ajax({
     type: "GET",
     url: `/api/scores/high/${user_id}/${quiz_id}`,
