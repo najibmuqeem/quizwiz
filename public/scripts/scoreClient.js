2034cab5a40b7eae29f32d5c380df6ab22f7ed61
Najib <najib.muqeem@gmail.com>
2020-03-13 03:59:55 +0000

    add ajax request for high score

--- public/scripts/scoreClient.js
+++ public/scripts/scoreClient.js
@@ -7,6 +7,16 @@ const storeScore = function(quiz_id, user_id, score) {
   });
 };
 
+// Get high score
+const getHighScore = function(user_id, quiz_id) {
+  console.log("i'm getting called");
+  $.ajax({
+    type: "GET",
+    url: `/api/scores/high/${user_id}/${quiz_id}`,
+    success: renderHighScore
+  });
+};
+
 // Get scores
 const getScores = function(quiz) {
   $.ajax({
